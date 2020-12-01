8ca537a95c35c5ba16a3de4142228c65a49abd4d
Vaibhav Gangrade <45598521+Viv31@users.noreply.github.com>
2020-12-01 19:16:59 +0530

    Add files via upload

---
+++ gallery.php
@@ -0,0 +1,85 @@
+<?php include_once('header.php'); ?>
+  <h3>Our Gallery</h3>
+  <p>In this example, the navigation bar is hidden on small screens and replaced by a button in the top right corner (try to re-size this window).</p>
+  <p>Only when the button is clicked, the navigation bar will be displayed.</p>
+  <p>Tip: You can also remove the .navbar-expand-md class to ALWAYS hide navbar links and display the toggler button.</p>
+  <div class="row">
+    <div class="col-md-3">
+      <div class="card">
+        <div class="card-header">Reading</div>
+        <div class="card-body">
+          <img src="reading.jpg" width="100%">
+        </div>
+         <div class="card-footer">View More</div>
+      </div>
+    </div>
+     <div class="col-md-3">
+      <div class="card">
+        <div class="card-header">Reading</div>
+        <div class="card-body">
+          <img src="reading.jpg" width="100%">
+        </div>
+         <div class="card-footer">View More</div>
+      </div>
+    </div>
+    <div class="col-md-3">
+      <div class="card">
+        <div class="card-header">Reading</div>
+        <div class="card-body">
+          <img src="reading.jpg" width="100%">
+        </div>
+         <div class="card-footer">View More</div>
+      </div>
+    </div>
+    <div class="col-md-3">
+      <div class="card">
+        <div class="card-header">Reading</div>
+        <div class="card-body">
+          <img src="reading.jpg" width="100%">
+        </div>
+         <div class="card-footer">View More</div>
+      </div>
+    </div>
+  </div>
+  <div class="row" style="padding: 5px;"></div>
+  <div class="row">
+    <div class="col-md-3">
+      <div class="card">
+        <div class="card-header">Reading</div>
+        <div class="card-body">
+          <img src="reading.jpg" width="100%">
+        </div>
+         <div class="card-footer">View More</div>
+      </div>
+    </div>
+     <div class="col-md-3">
+      <div class="card">
+        <div class="card-header">Reading</div>
+        <div class="card-body">
+          <img src="reading.jpg" width="100%">
+        </div>
+         <div class="card-footer">View More</div>
+      </div>
+    </div>
+    <div class="col-md-3">
+      <div class="card">
+        <div class="card-header">Reading</div>
+        <div class="card-body">
+          <img src="reading.jpg" width="100%">
+        </div>
+         <div class="card-footer">View More</div>
+      </div>
+    </div>
+    <div class="col-md-3">
+      <div class="card">
+        <div class="card-header">Reading</div>
+        <div class="card-body">
+          <img src="reading.jpg" width="100%">
+        </div>
+         <div class="card-footer">View More</div>
+      </div>
+    </div>
+  </div>
+<?php include_once('footer.php'); ?>
+
+
---
+++ our-services.php
@@ -0,0 +1,17 @@
+<?php include_once('header.php'); ?>
+  <h3>Our Services:</h3>
+<ul>
+    <li>Lorem ipsum dolor sit amet,</li>
+    <li>consectetur adipisicing elit, sed do eiusmod</li>
+    <li>tempor incididunt ut labore et dolore magna aliqua.</li>
+    <li> Ut enim ad minim veniam,
+  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo</li>
+  <li>consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse</li>
+  <li>cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+</li>
+<li>proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</li>
+  </ul>
+  
+<?php include_once('footer.php'); ?>
+
+
